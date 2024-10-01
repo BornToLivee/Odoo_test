27db691fb8cb90737fae602f911b50958e8594c8
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-10-01 11:12:29 +0300

    create Person model

---
+++ custom_addons/person_management/models/person.py
@@ -0,0 +1,47 @@
+from odoo import models, fields, api
+from datetime import date
+
+
+class Persons(models.Model):
+    _name = "person.management"
+    _description = "Person Management"
+
+    first_name = fields.Char(string="First Name", required=True)
+    last_name = fields.Char(string="Last Name", required=True)
+    full_name = fields.Char(
+        string="Full Name", compute="_compute_full_name", store=True
+    )
+    birthday = fields.Date(string="Birthday")
+    age = fields.Integer(string="Age", compute="_compute_age", store=True)
+    sex = fields.Selection(
+        [("male", "Male"), ("female", "Female"), ("non_binary", "Non-Binary")],
+        string="Sex",
+        required=True,
+    )
+    company_id = fields.Many2one(
+        "res.company",
+        string="Company",
+        required=True,
+        default=lambda self: self.env.company,
+    )
+
+    @api.depends("first_name", "last_name")
+    def _compute_full_name(self):
+        for person in self:
+            person.full_name = f"{person.first_name} {person.last_name}"
+
+    @api.depends("birthday")
+    def _compute_age(self):
+        for person in self:
+            if person.birthday:
+                today = date.today()
+                person.age = (
+                    today.year
+                    - person.birthday.year
+                    - (
+                        (today.month, today.day)
+                        < (person.birthday.month, person.birthday.day)
+                    )
+                )
+            else:
+                person.age = 0
